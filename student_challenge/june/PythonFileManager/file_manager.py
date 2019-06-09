cf644ca56bb5904269567c0279b6318c87e826e9
tstaunton <tony.staunton@gmail.com>
2019-06-09 10:21:04 +0100

    Update file_manager.py

--- student_challenge/june/PythonFileManager/file_manager.py
+++ student_challenge/june/PythonFileManager/file_manager.py
@@ -1,5 +1,7 @@
 # June Student Challenge
 
+# Import necessary modules.
+# The OS and Subprocess modules deal with the operating system
 import shutil        
 import os        
 import time
@@ -10,4 +12,15 @@ def Read():
     file=open(path,"r")
     print(file.read()) 
     input('Press Enter...')  
-    file.close()
+    file.close() 
+
+
+def Write():    
+    path=input("Enter the path of file to write or create:")
+    if os.path.isfile(path):
+        print('Rebuilding the existing file') 
+    else:
+        print('Creating the new file') 
+    text=input("Enter text:")
+    file=open(path,"w")
+    file.write(text)
